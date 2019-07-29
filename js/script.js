d27d1f2f0570a93bf34db028a34d647005d68c5e
manuel.bit <emmanuel_njuguna@outlook.com>
2019-07-29 08:02:57 +0300

    add new pizza construct

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 //business ogic
-var pizza ={
-  size['large', 'medium', 'small'],
-  flavours['peperoni', 'double-cheese', 'BBQ', 'huwaiian'],
-  toppings['peperoni', 'MUSHROOM','MOZARELLA'],
-  crust['CRISPY','STUFFED','GLUTEN','meaty']
-}
 function pizza(size,flavours,toppings,crust){
   this.size= size;
   this.flavours= flavours;
@@ -15,5 +9,13 @@ function pizza(size,flavours,toppings,crust){
 $(document).ready(function(){
   $(form#new-pizza).submit(function(event){
     event.preventDefault();
+
+    var pizaSize = $("#pizza-size option:selected").val();
+    var pizaFlavour =$("#pizza-type option:selected").val();
+    var pizatop =$("#pizza-toppings option:selected").val();
+    var pizaCrust = $("#pizza-crust option:selected").val();
+
+    var newPizza=newPizza(pizaSize, pizaFlavour, pizatop, pizaCrust);
   })
+
 }
